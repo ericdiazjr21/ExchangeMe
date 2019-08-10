8aa1d69e9e14d423050de87278c312d4b6526a1b
Eric Diaz <ericdiaz@pursuit.org>
2019-08-10 01:26:42 -0400

    added internet permission, created repository tests, updated network tests

--- app/src/main/java/ericdiaz/program/currencyconveterlive2019/model/ExchangeRateResponse.java
+++ app/src/main/java/ericdiaz/program/currencyconveterlive2019/model/ExchangeRateResponse.java
@@ -4,18 +4,18 @@ import com.google.gson.annotations.SerializedName;
 
 public final class ExchangeRateResponse {
 
-    private final Rates[] rates;
+    private final Rates rates;
     @SerializedName("base")
     private final String baseCurrency;
     private final String date;
 
-    public ExchangeRateResponse(Rates[] rates, String baseCurrency, String date) {
+    public ExchangeRateResponse(Rates rates, String baseCurrency, String date) {
         this.rates = rates;
         this.baseCurrency = baseCurrency;
         this.date = date;
     }
 
-    public Rates[] getRates() {
+    public Rates getRates() {
         return rates;
     }
 
--- app/src/test/java/ericdiaz/program/currencyconveterlive2019/NetworkTest.java
+++ app/src/test/java/ericdiaz/program/currencyconveterlive2019/NetworkTest.java
@@ -1,49 +1,23 @@
 package ericdiaz.program.currencyconveterlive2019;
 
-import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
-import ericdiaz.program.currencyconveterlive2019.model.ExchangeRateResponse;
 import ericdiaz.program.currencyconveterlive2019.network.ExchangeRateService;
-import io.reactivex.Single;
-import io.reactivex.disposables.CompositeDisposable;
-
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
+import ericdiaz.program.currencyconveterlive2019.network.RetrofitServiceGenerator;
 
 
 public class NetworkTest {
 
-    @Mock
-    private ExchangeRateService exchangeRateService;
-    private CompositeDisposable compositeDisposable;
-
     @Before
     public void setUp() {
-        MockitoAnnotations.initMocks(this);
-        compositeDisposable = new CompositeDisposable();
     }
 
     @Test
-    public void testExchangeRateServiceResponse() {
-        //given
-        ExchangeRateResponse response = new ExchangeRateResponse(null, "USD", "10-10-1992");
-
-        //when
-        when(exchangeRateService.getExchangeRates(null, null))
-          .thenReturn(Single.just(response));
-
-        //then
-        compositeDisposable.add(exchangeRateService.getExchangeRates(null, null)
-          .subscribe(exchangeRateResponse -> assertEquals("USD", exchangeRateResponse.getBaseCurrency())));
-
+    public void testRetrofitServiceGenerator() {
+        Assert.assertTrue(RetrofitServiceGenerator.getExchangeRateService() instanceof ExchangeRateService);
     }
 
-    @After
-    public void tearDown() {
-        compositeDisposable.dispose();
-    }
+
 }
