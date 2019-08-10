551a5753da3b56410f101f11d0aa32e917f080f9
Eric Diaz <ericdiaz@pursuit.org>
2019-08-09 23:03:01 -0400

    updated Retrofit class name, cleane up code

--- app/src/main/java/ericdiaz/program/currencyconveterlive2019/network/RetrofitServiceGenerator.java
+++ app/src/main/java/ericdiaz/program/currencyconveterlive2019/network/RetrofitServiceGenerator.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public final class RetrofitGenerator {
+public final class RetrofitServiceGenerator {
 
     private static Retrofit singleInstance;
 
--- app/src/main/java/ericdiaz/program/currencyconveterlive2019/repository/ExchangeRateRepository.java
+++ app/src/main/java/ericdiaz/program/currencyconveterlive2019/repository/ExchangeRateRepository.java
@@ -3,13 +3,14 @@ package ericdiaz.program.currencyconveterlive2019.repository;
 import android.support.annotation.NonNull;
 
 import ericdiaz.program.currencyconveterlive2019.model.ExchangeRateResponse;
-import ericdiaz.program.currencyconveterlive2019.network.RetrofitGenerator;
+import ericdiaz.program.currencyconveterlive2019.network.RetrofitServiceGenerator;
 import io.reactivex.Single;
 
 public class ExchangeRateRepository implements BaseRepository {
 
     @Override
-    public Single<ExchangeRateResponse> requestExchangeRates(@NonNull String date, @NonNull String baseCurrency) {
-        return RetrofitGenerator.getExchangeRateService().getExchangeRates(date,baseCurrency);
+    public Single<ExchangeRateResponse> requestExchangeRates(@NonNull final String date,
+                                                             @NonNull final String baseCurrency) {
+        return RetrofitServiceGenerator.getExchangeRateService().getExchangeRates(date,baseCurrency);
     }
 }
