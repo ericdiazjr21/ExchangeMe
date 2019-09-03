9290b74d6fd1e902a78b0b47965f0f07f991a9df
Eric Diaz <ericdiaz@pursuit.org>
2019-09-02 20:26:02 -0400

    updated tests

--- app/src/test/java/ericdiaz/program/currencyconveterlive2019/RepositoryTest.java
+++ app/src/test/java/ericdiaz/program/currencyconveterlive2019/RepositoryTest.java
@@ -1,7 +1,6 @@
 package ericdiaz.program.currencyconveterlive2019;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -44,36 +43,7 @@ public class RepositoryTest {
         verify(exchangeRateRepository).requestExchangeRates(date, baseCurrency);
 
     }
-
-    @Test
-    public void testRequestExchangeRatesMethod() {
-        //given
-        String date = "2000-10-10";
-        String baseCurrency = "USD";
-
-        //when
-        Single<ExchangeRateResponse> exchangeRateResponse = exchangeRateRepository.requestExchangeRates(date, baseCurrency);
-
-        //then
-        compositeDisposable.add(exchangeRateResponse.subscribe(response ->
-          Assert.assertEquals("USD", response.getBaseCurrency())));
-    }
-
-    @Test
-    public void testRequestExchangeRatesMethodWithDate2010_10_10() {
-        //given
-        String date = "2010-10-10";
-        String baseCurrency = "USD";
-        double verifiedCanadianDollarValue = 1.0211907164;
-
-        //when
-        Single<ExchangeRateResponse> exchangeRateResponse = exchangeRateRepository.requestExchangeRates(date, baseCurrency);
-
-        //then
-        compositeDisposable.add(exchangeRateResponse.subscribe(response ->
-          Assert.assertEquals(verifiedCanadianDollarValue, response.getRates().getCanadianDollar(), 0)));
-    }
-
+    
     @After
     public void tearDown() {
         compositeDisposable.dispose();
