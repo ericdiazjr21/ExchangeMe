4ef55cf262a7e8a347ceeddace4f9e5387d9e3ec
Eric Diaz <ericdiaz@pursuit.org>
2019-09-27 00:08:12 -0400

    added baseCurrency spinner

--- app/src/main/java/ericdiaz/program/currencyconveterlive2019/view/ConversionActivity.java
+++ app/src/main/java/ericdiaz/program/currencyconveterlive2019/view/ConversionActivity.java
@@ -2,7 +2,10 @@ package ericdiaz.program.currencyconveterlive2019.view;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.ViewGroup;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,6 +24,25 @@ public class ConversionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_conversion);
 
         DialPad dialPad = findViewById(R.id.dial_pad_view);
+
+        Spinner baseCurrencySpinner = findViewById(R.id.base_currency_spinner);
+        ArrayAdapter<CharSequence> baseCurrencyAdapter = ArrayAdapter
+          .createFromResource(
+            this,
+            R.array.currency_list,
+            R.layout.support_simple_spinner_dropdown_item);
+        baseCurrencySpinner.setAdapter(baseCurrencyAdapter);
+        baseCurrencySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         dialPad.setOnDialPressedListener(dial -> Log.d(TAG, "onCreate: " + dial));
     }
