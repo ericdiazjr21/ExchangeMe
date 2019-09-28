be48464e2267ea52d88f0aaef59ee3bdbc7c16aa
Eric Diaz <ericdiaz@pursuit.org>
2019-09-28 19:34:07 -0400

    added empty companion objectto model

--- data/src/main/java/ericdiaz/program/data/model/ExchangeRateResponse.kt
+++ data/src/main/java/ericdiaz/program/data/model/ExchangeRateResponse.kt
@@ -3,8 +3,12 @@ package ericdiaz.program.data.model
 import com.google.gson.annotations.SerializedName
 
 data class ExchangeRateResponse(@SerializedName("base")
-                                   val baseCurrency: String,
+                                val baseCurrency: String,
 
                                 val ratesMap: Map<String, Double>,
 
-                                val date: String)
+                                val date: String) {
+    companion object {
+        val EMPTY = ExchangeRateResponse("emptyBaseCurrency", emptyMap(), "emptyDate")
+    }
+}
