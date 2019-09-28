786f1e353221ba5112f8f7623e165395f590ab19
Eric Diaz <ericdiaz@pursuit.org>
2019-09-28 19:10:21 -0400

    created new kotlin response model for API

---
+++ data/src/main/java/ericdiaz/program/data/model/ExchangeRateResponse.kt
@@ -0,0 +1,10 @@
+package ericdiaz.program.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class ExchangeRateResponse(@SerializedName("base")
+                                   val baseCurrency: String,
+
+                                val ratesMap: Map<String, Double>,
+
+                                val date: String)
