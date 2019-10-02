638abd4227f91c2f0661a7468e56ee69d4f7cb4b
Eric Diaz <ericdiaz@pursuit.org>
2019-10-01 23:17:17 -0400

    added basic decimal formatter and tests

--- app/src/main/java/ericdiaz/program/currencyconveterlive2019/view/dialpad/DialPadConductor.kt
+++ app/src/main/java/ericdiaz/program/currencyconveterlive2019/view/dialpad/DialPadConductor.kt
@@ -1,59 +1,90 @@
 package ericdiaz.program.currencyconveterlive2019.view.dialpad
 
-import android.util.Log
 import android.widget.TextView
-import ericdiaz.program.currencyconveterlive2019.extensions.cashAppCurrencyFormat
-import ericdiaz.program.currencyconveterlive2019.extensions.cashAppCurrencyFormatParser
+import ericdiaz.program.currencyconveterlive2019.extensions.*
 
-class DialPadConductor(private val receiverTextView: TextView) : OnDialPressedListener {
+class DialPadConductor(private val receiverTextView: TextView,
+                       private val formatMode: Int = BASIC_DECIMAL_FORMAT) : OnDialPressedListener {
+
+    companion object {
+        const val BASIC_DECIMAL_FORMAT = 1
+        const val CURRENCY_SYMBOL_FORMAT = 2
+    }
 
     init {
-        receiverTextView.text = zeroAmount()
+        receiverTextView.text =
+                when (formatMode) {
+                    BASIC_DECIMAL_FORMAT -> zeroDecimalFormat()
+                    CURRENCY_SYMBOL_FORMAT -> zeroCurrencySymbolFormat()
+                    else -> throw IllegalArgumentException("Invalid format mode input")
+                }
     }
 
     override fun onDialPressed(dial: Dial) {
 
-        val baseCurrencyAmountText = receiverTextView.text.toString().trim()
-        val unformattedBaseCurrency = baseCurrencyAmountText.cashAppCurrencyFormatParser()
+        with(getReceiverViewText()) {
 
-        with(unformattedBaseCurrency) {
+            when (formatMode) {
 
-            Log.d("DialConductor", this)
+                BASIC_DECIMAL_FORMAT ->
+                    getNewFormattedDecimalTextAmount(this.toDouble(), dial.dialSymbol)
+                            .let { receiverTextView.text = it }
 
-            val dialSymbol = dial.dialSymbol
+                CURRENCY_SYMBOL_FORMAT ->
+                    getNewFormattedCurrencyTextAmount(this.currencySymbolFormatParser(), dial.dialSymbol)
+                            .let { receiverTextView.text = it }
 
-            if (dial is Dial.Delete) {
-
-                if (this != "0" && this.length - 1 != 0) {
-                    this.subSequence(0..this.length - 2)
-                            .toString()
-                            .toInt()
-                            .cashAppCurrencyFormat()
-                } else {
-                    zeroAmount()
-                }
-            } else {
-                when (this) {
-                    "" -> zeroAmount()
-
-                    "0" -> this.replace("0", dialSymbol)
-                            .toInt()
-                            .cashAppCurrencyFormat()
-
-                    else ->
-                        try {
-                            (this + dialSymbol)
-                                    .toInt()
-                                    .cashAppCurrencyFormat()
-                        } catch (e: NumberFormatException) {
-                            Int.MAX_VALUE.cashAppCurrencyFormat()
-                        }
-                }
-            }.let {
-                receiverTextView.text = it
+                else -> throw IllegalArgumentException("Invalid format mode input")
             }
         }
     }
 
-    private fun zeroAmount() = 0.cashAppCurrencyFormat()
+    private fun getNewFormattedDecimalTextAmount(initialDecimalValue: Double,
+                                                 dialSymbol: String): String {
+        return if (dialSymbol == Dial.Delete.dialSymbol) {
+            initialDecimalValue.decreaseDecimalValue().toString()
+        } else {
+            initialDecimalValue.increaseDecimalValue(dialSymbol).toString()
+        }
+    }
+
+    private fun getNewFormattedCurrencyTextAmount(unformattedAmountText: String,
+                                                  dialSymbol: String): String {
+        return if (dialSymbol == Dial.Delete.dialSymbol) {
+
+            if (unformattedAmountText != "0" && unformattedAmountText.length - 1 != 0) {
+                unformattedAmountText.subSequence(0..unformattedAmountText.length - 2)
+                        .toString()
+                        .toInt()
+                        .currencySymbolFormat()
+            } else {
+                zeroCurrencySymbolFormat()
+            }
+        } else {
+            when (unformattedAmountText) {
+                "" -> zeroCurrencySymbolFormat()
+
+                "0" -> unformattedAmountText.replace("0", dialSymbol)
+                        .toInt()
+                        .currencySymbolFormat()
+
+                else ->
+                    try {
+                        (unformattedAmountText + dialSymbol)
+                                .toInt()
+                                .currencySymbolFormat()
+                    } catch (e: NumberFormatException) {
+                        Int.MAX_VALUE.currencySymbolFormat()
+                    }
+            }
+        }
+    }
+
+    private fun getReceiverViewText(): String {
+        return receiverTextView.text.toString().trim()
+    }
+
+    private fun zeroCurrencySymbolFormat() = 0.currencySymbolFormat()
+
+    private fun zeroDecimalFormat() = 0.basicDecimalFormat()
 }
--- app/src/test/java/ericdiaz/program/currencyconveterlive2019/ExtensionsTest.kt
+++ app/src/test/java/ericdiaz/program/currencyconveterlive2019/ExtensionsTest.kt
@@ -1,7 +1,9 @@
 package ericdiaz.program.currencyconveterlive2019
 
 import com.google.common.truth.Truth.assertThat
+import ericdiaz.program.currencyconveterlive2019.extensions.decreaseDecimalValue
 import ericdiaz.program.currencyconveterlive2019.extensions.getExchangeValue
+import ericdiaz.program.currencyconveterlive2019.extensions.increaseDecimalValue
 import org.junit.Test
 
 class ExtensionsTest {
@@ -18,5 +20,142 @@ class ExtensionsTest {
 
         //then
         assertThat(actualResult).matches(expectedResult)
+    }
+
+
+    @Test
+    fun basicIncrease2() {
+        val inputDouble = 100.00
+        val expectedResult = 10000.98
+
+        val actualResult = inputDouble
+                        .increaseDecimalValue("9")
+                        .increaseDecimalValue("8")
+
+        assertThat(actualResult).isEqualTo(expectedResult)
+    }
+
+    @Test
+    fun basicIncrease4() {
+        val inputDouble = 0.00
+        val expectedResult = 98.76
+
+        val actualResult = inputDouble
+                .increaseDecimalValue("9")
+                .increaseDecimalValue("8")
+                .increaseDecimalValue("7")
+                .increaseDecimalValue("6")
+
+        assertThat(actualResult).isEqualTo(expectedResult)
+    }
+
+    @Test
+    fun basicIncrease8() {
+        val inputDouble = 0.00
+        val expectedResult = 987654.32
+
+        val actualResult = inputDouble
+                .increaseDecimalValue("9")
+                .increaseDecimalValue("8")
+                .increaseDecimalValue("7")
+                .increaseDecimalValue("6")
+                .increaseDecimalValue("5")
+                .increaseDecimalValue("4")
+                .increaseDecimalValue("3")
+                .increaseDecimalValue("2")
+
+        assertThat(actualResult).isEqualTo(expectedResult)
+    }
+
+    @Test
+    fun basicTextFormatterExtensions() {
+        val inputDouble = 100.00
+        val expectedResult = 10.00
+
+        val actualResult = inputDouble.decreaseDecimalValue()
+
+        assertThat(actualResult).isEqualTo(expectedResult)
+    }
+
+    @Test
+    fun basicDecrease() {
+        val inputDouble = 10000.98
+        val expectedResult = 1000.09
+
+        val actualResult = inputDouble.decreaseDecimalValue()
+
+        assertThat(actualResult).isEqualTo(expectedResult)
+    }
+
+    @Test
+    fun basicDecrease1() {
+        val inputDouble = 10023.44
+        val expectedResult = 1002.34
+
+        val actualResult = inputDouble.decreaseDecimalValue()
+
+        assertThat(actualResult).isEqualTo(expectedResult)
+    }
+
+    @Test
+    fun basicDecrease2() {
+        val inputDouble = 11223.44
+        val expectedResult = 112.23
+
+        val actualResult = inputDouble
+                .decreaseDecimalValue()
+                .decreaseDecimalValue()
+
+        assertThat(actualResult).isEqualTo(expectedResult)
+    }
+
+    @Test
+    fun basicDecrease5() {
+        val inputDouble = 11223.44
+        val expectedResult = 0.11
+
+        val actualResult = inputDouble
+                .decreaseDecimalValue()
+                .decreaseDecimalValue()
+                .decreaseDecimalValue()
+                .decreaseDecimalValue()
+                .decreaseDecimalValue()
+
+        assertThat(actualResult).isEqualTo(expectedResult)
+    }
+
+    @Test
+    fun basicDecrease7() {
+        val inputDouble = 11223.44
+        val expectedResult = 0.0
+
+        val actualResult = inputDouble
+                .decreaseDecimalValue()
+                .decreaseDecimalValue()
+                .decreaseDecimalValue()
+                .decreaseDecimalValue()
+                .decreaseDecimalValue()
+                .decreaseDecimalValue()
+                .decreaseDecimalValue()
+
+        assertThat(actualResult).isEqualTo(expectedResult)
+    }
+
+    @Test
+    fun basicDecrease8() {
+        val inputDouble = 987654.32
+        val expectedResult = 0.0
+
+        val actualResult = inputDouble
+                .decreaseDecimalValue()
+                .decreaseDecimalValue()
+                .decreaseDecimalValue()
+                .decreaseDecimalValue()
+                .decreaseDecimalValue()
+                .decreaseDecimalValue()
+                .decreaseDecimalValue()
+                .decreaseDecimalValue()
+
+        assertThat(actualResult).isEqualTo(expectedResult)
     }
 }
