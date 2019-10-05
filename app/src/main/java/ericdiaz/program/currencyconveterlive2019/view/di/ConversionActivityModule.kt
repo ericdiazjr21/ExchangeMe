3a82852579be3bd56e2efc9980387699429bace9
Eric Diaz <ericdiaz@pursuit.org>
2019-10-04 23:36:35 -0400

    WIP with view binding and moving array adapter to DI

--- app/src/main/java/ericdiaz/program/currencyconveterlive2019/view/di/ConversionActivityModule.kt
+++ app/src/main/java/ericdiaz/program/currencyconveterlive2019/view/di/ConversionActivityModule.kt
@@ -1,7 +1,12 @@
 package ericdiaz.program.currencyconveterlive2019.view.di
 
+import android.content.Context
+import android.widget.ArrayAdapter
 import dagger.Module
+import dagger.Provides
 import dagger.android.ContributesAndroidInjector
+import ericdiaz.program.currencyconveterlive2019.R
+import ericdiaz.program.currencyconveterlive2019.di.CurrencyConverterApplication
 import ericdiaz.program.currencyconveterlive2019.view.ConversionActivity
 import ericdiaz.program.currencyconveterlive2019.viewmodel.di.ViewModelModule
 
@@ -15,4 +20,18 @@ abstract class ConversionActivityModule {
     @ConversionActivityScope
     @ContributesAndroidInjector(modules = [ViewModelModule::class])
     abstract fun conversionActivity(): ConversionActivity
+
+    @Module
+    companion object {
+
+        @Provides
+        @JvmStatic
+        fun providesCurrencyListArray(application: CurrencyConverterApplication): ArrayAdapter<CharSequence> {
+            return ArrayAdapter.createFromResource(
+                    application.applicationContext,
+                    R.array.currency_list,
+                    R.layout.support_simple_spinner_dropdown_item
+            )
+        }
+    }
 }
