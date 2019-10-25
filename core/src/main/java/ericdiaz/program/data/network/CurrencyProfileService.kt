b8df886d36e07becb0013e9c71a02ae059649038
Eric Diaz <ericdiaz@pursuit.org>
2019-10-24 21:29:14 -0400

    got base currency edittext to scroll left to add more numbers

--- core/src/main/java/ericdiaz/program/data/network/CurrencyProfileService.kt
+++ core/src/main/java/ericdiaz/program/data/network/CurrencyProfileService.kt
@@ -6,6 +6,6 @@ import retrofit2.http.GET
 
 interface CurrencyProfileService {
 
-    @GET("ericdiazjr21/c41446b8d723a6ac616df65cd134e647/raw/200e6107a2e6a61022fc9161842dcf088c1c162f/currency_converter_server.txt")
+    @GET("ericdiazjr21/c41446b8d723a6ac616df65cd134e647/raw/c53d5734029c3f14ee2b420453cadda3e6cf0780/currency_converter_server.txt")
     fun getCurrencyProfileMap(): Single<Map<String, CurrencyProfile>>
 }
