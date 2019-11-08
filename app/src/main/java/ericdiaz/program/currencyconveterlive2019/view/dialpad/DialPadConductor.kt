12af2fd8b4afb30fd6aaa8bc6bd5df5bf78a0490
Eric Diaz <ericdiaz@pursuit.org>
2019-11-08 15:02:01 -0500

    added clear button logic

--- app/src/main/java/ericdiaz/program/currencyconveterlive2019/view/dialpad/DialPadConductor.kt
+++ app/src/main/java/ericdiaz/program/currencyconveterlive2019/view/dialpad/DialPadConductor.kt
@@ -1,44 +1,35 @@
 package ericdiaz.program.currencyconveterlive2019.view.dialpad
 
 import android.widget.TextView
-import androidx.core.widget.TextViewCompat
 import ericdiaz.program.currencyconveterlive2019.extensions.currencySymbolFormat
 import ericdiaz.program.currencyconveterlive2019.extensions.currencySymbolFormatParser
 import ericdiaz.program.currencyconveterlive2019.extensions.decreaseDecimalValue
 import ericdiaz.program.currencyconveterlive2019.extensions.increaseDecimalValue
 
-class DialPadConductor(private val receiverTextView: TextView,
-                       private val formatMode: Int = BASIC_DECIMAL_FORMAT) : OnDialPressedListener {
+class DialPadConductor(
+        private val connectedView: TextView,
+        private val dialResponseListener: DialResponseListener,
+        private val formatMode: Int = BASIC_DECIMAL_FORMAT) : OnDialPressedListener {
 
     companion object {
         const val BASIC_DECIMAL_FORMAT = 1
         const val CURRENCY_SYMBOL_FORMAT = 2
     }
 
-    init {
-        receiverTextView.apply {
-            this.text =
-                    when (formatMode) {
-                        BASIC_DECIMAL_FORMAT, CURRENCY_SYMBOL_FORMAT -> zeroDecimalFormat()
-                        else -> throw IllegalArgumentException("Invalid format mode input")
-                    }
-            TextViewCompat.setAutoSizeTextTypeWithDefaults(this, TextViewCompat.AUTO_SIZE_TEXT_TYPE_UNIFORM)
-        }
-    }
-
     override fun onDialPressed(dial: Dial) {
 
-        with(getReceiverViewText()) {
+        if (dial == Dial.Clear) dialResponseListener.onChange("clear")
+        else with(connectedView.text.toString()) {
 
             when (formatMode) {
 
                 BASIC_DECIMAL_FORMAT ->
                     getNewFormattedDecimalTextAmount(this.toDouble(), dial.dialSymbol)
-                            .let { receiverTextView.text = it }
+                            .let { dialResponseListener.onChange(it) }
 
                 CURRENCY_SYMBOL_FORMAT ->
                     getNewFormattedCurrencyTextAmount(this.currencySymbolFormatParser(), dial.dialSymbol)
-                            .let { receiverTextView.text = it }
+                            .let { dialResponseListener.onChange(it) }
 
                 else -> throw IllegalArgumentException("Invalid format mode input")
             }
@@ -88,10 +79,6 @@ class DialPadConductor(private val receiverTextView: TextView,
                     }
             }
         }
-    }
-
-    private fun getReceiverViewText(): String {
-        return receiverTextView.text.toString().trim()
     }
 
     private fun zeroDecimalFormat() = "0.00"
